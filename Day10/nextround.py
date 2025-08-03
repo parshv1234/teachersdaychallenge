05246c16a6b44b5a2fdd999ebee3f2e30df3c7f2
parshv1234 <modiparshv2005@gmail.com>
2025-08-03 20:49:59 +0530

    Day10 Problem

--- Day10/nextround.py
+++ Day10/nextround.py
@@ -11,7 +11,6 @@ The first line of the input contains two integers n and k (1 ≤ k ≤ n
 
 The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
 
-Output
 Output the number of participants who advance to the next round.
 '''
 # class Solution:
