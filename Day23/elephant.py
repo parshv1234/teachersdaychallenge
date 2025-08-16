a5a0e24b30ef25b9c9229fc28f4c26aa963c411d
parshv1234 <modiparshv2005@gmail.com>
2025-08-16 21:39:09 +0530

    Day23 Problem

---
+++ Day23/elephant.py
@@ -0,0 +1,35 @@
+'''
+A. Elephant
+time limit per test1 second
+memory limit per test256 megabytes
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. 
+In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+
+Input
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+
+Output
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+
+'''
+
+x = int(input())
+steps = 0
+
+while x > 0:
+    if x >= 5:
+        x -= 5
+        steps += 1
+    elif x >= 4:
+        x -= 4
+        steps += 1
+    elif x >= 3:
+        x -= 3
+        steps += 1
+    elif x >= 2:
+        x -= 2
+        steps += 1
+    else:
+        x -= 1
+        steps += 1
+print(steps)
